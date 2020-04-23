480c4f5decbb882e82fea8f9e283fc4a96ae4ab2
YTK <yu.nakkano@gmail.com>
2020-04-24 00:03:41 +0900

    add empty actions to message_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: true
   has_many :group_users
+  has_many :messages
   has_many :groups, through: :group_users
 end
