b408a126b35bbc2ef7e9f8f9b261c261b0ba7800
YTK <yu.nakkano@gmail.com>
2020-04-24 15:46:54 +0900

    create image_uploader model and edit
    rails g uploader image
    ->
    edit model file for carrierewave

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,22 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+  process convert: 'jpg'
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process resize_to_fit: [800, 800]
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    "#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.jpg" if original_filename.present?
+  end
+end
