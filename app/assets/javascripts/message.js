5e6b2697914ac436dafd9459cea8f0df37267e8b
YTK <yu.nakkano@gmail.com>
2020-05-02 13:25:12 +0900

    Implement form-reset / auto-scroll after sending a message. Refactored html variable in buildHTML function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,49 +1,29 @@
 $(function(){
   function buildHTML(msg){
-    if (msg.text && msg.image){
-      var html = `<div class="chat-main__message-list__message">
-                    <div class="chat-main__message-list__message__upper-box">
-                      <p class="chat-main__message-list__message__upper-box__talker">
-                      ${msg.user_name}
-                      </p>
-                      <p class="chat-main__message-list__message__upper-box__datetime">
-                      ${msg.created_at}
-                      </p>
-                      </div>
-                      <div class="chat-main__message-list__message__text">
-                      ${msg.text}
+    var html_upper_part = `<div class="chat-main__message-list__message">
+                            <div class="chat-main__message-list__message__upper-box">
+                              <p class="chat-main__message-list__message__upper-box__talker">
+                              ${msg.user_name}
+                              </p>
+                              <p class="chat-main__message-list__message__upper-box__datetime">
+                              ${msg.created_at}
+                              </p>
+                            </div>`
+    var html_text_part = `<div class="chat-main__message-list__message__text">
+                            ${msg.text}`
+    if ( msg.image ){
+      var html = `${html_upper_part}
+                    ${html_text_part}
                       <img class="chat-main__message-list__message__image" src="${msg.image}">
                     </div>
                   </div>`
     } else if(msg.text) {
-      var html = `<div class="chat-main__message-list__message">
-                    <div class="chat-main__message-list__message__upper-box">
-                      <p class="chat-main__message-list__message__upper-box__talker">
-                      ${msg.user_name}
-                      </p>
-                      <p class="chat-main__message-list__message__upper-box__datetime">
-                      ${msg.created_at}
-                      </p>
-                    </div>
-                    <div class="chat-main__message-list__message__text">
-                    ${msg.text}
-                    </div>
-                  </div>`
-    } else{
-      var html = `<div class="chat-main__message-list__message">
-                    <div class="chat-main__message-list__message__upper-box">
-                      <p class="chat-main__message-list__message__upper-box__talker">
-                      ${msg.user_name}
-                      </p>
-                      <p class="chat-main__message-list__message__upper-box__datetime">
-                      ${msg.created_at}
-                      </p>
-                    </div>
-                    <div class="chat-main__message-list__message__text">
-                      <img class="chat-main__message-list__message__image" src="${json.image}">
+      var html = `${html_upper_part}
+                    ${html_text_part}
                     </div>
                   </div>`
     }
+    return html
   }
 
   $('.chat-main__message-form__form').on('submit',function(e){
@@ -61,11 +41,12 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.chat-main__message-list').append(html);
-      $('.chat-main__message-form__form__input-box__text').val('');
+      $('form')[0].reset();
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
       $('.chat-main__message-form__form__submit').prop('disabled', false);
     })
     .fail(function(){
-      alert('error');
+      alert('Error occured when sending the message');
     })
   })
 });
