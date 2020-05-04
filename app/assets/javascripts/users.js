f9ce5fc6fc479379111e283532489d16add3863a
YTK <yu.nakkano@gmail.com>
2020-05-04 15:47:46 +0900

    add users.js json data form and done/fail codes for uername search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,25 @@
 $(function(){
+  function appendUser(user){
+    console.log(user);
+  }
+  function appendErrMsgToHTML(msg){
+    console.log(msg);
+  }
 
   $('#user-search-field').on("keyup", function(e){
     e.preventDefault();
     var input = $("#user-search-field").val();
-    
-  })
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { key: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      console.log('Success');
+    })
+    .fail(function(){
+      alert('error');
+    });
+  });
 });
